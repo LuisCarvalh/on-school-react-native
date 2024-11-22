509c71538c6e3147db12ba56307094fea39e6f2d
Raquel Sarni <raquelsarin@hotmail.com>
2024-11-22 16:17:50 -0300

    Add files via upload
    página de leitura de post

---
+++ navigation/AppNavigator.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from '../screens/Login';
+import Post from '../screens/Post';
+import PostDetails from '../screens/DetailsPost';
+
+const Stack = createStackNavigator();
+
+export default function AppNavigator() {
+  return (
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Post" component={Post} />
+        <Stack.Screen name="PostDetails" component={PostDetails} />
+      </Stack.Navigator>
+  );
+}
